35d042d07d7cfe77b7bf3723b773a4cf4d14207f
joemiddie13 <joemiddie13@gmail.com>
2023-12-04 14:17:17 -0800

    Completed challenges before stretch challenge

--- CoffeeBar.py
+++ CoffeeBar.py
@@ -1,16 +1,13 @@
 class CoffeeBar:
-    def __init__(self, name, barista=None):
-        self.name = name
-        self.orders_list = []
-        self.barista = barista
+  def __init__(self, name, barista=None):
+    self.name = name
+    self.orders_list = []
+    self.barista = barista
 
-    def place_order(self, order):
-        self.orders_list.append(order)
+  def place_order(self, order):
+    self.orders_list.append(order)
 
-    def process_orders(self):
-        if self.barista is not None:
-            greeting = self.barista.greeting
-        else:
-            greeting = ""
-        for order in self.orders_list:
-            print(f"{greeting} {order.to_string()}")
+  def process_orders(self):
+    for order in self.orders_list:
+      greeting = f"{self.barista.greeting}" if self.barista else ""
+      print(f"{greeting} {order.to_string()}")
