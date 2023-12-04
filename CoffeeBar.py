3ad6b471ceca31798366cf266259b38f667ede43
joemiddie13 <joemiddie13@gmail.com>
2023-12-04 14:09:17 -0800

    Added 'Kevin' as a Barista

--- CoffeeBar.py
+++ CoffeeBar.py
@@ -1,11 +1,16 @@
 class CoffeeBar:
-  def __init__(self, name):
-    self.name = name
-    self.orders_list = []
-  
-  def place_order(self, order):
-    self.orders_list.append(order)
+    def __init__(self, name, barista=None):
+        self.name = name
+        self.orders_list = []
+        self.barista = barista
 
-  def process_orders(self):
-    for order in self.orders_list:
-      print(order.to_string())
+    def place_order(self, order):
+        self.orders_list.append(order)
+
+    def process_orders(self):
+        if self.barista is not None:
+            greeting = self.barista.greeting
+        else:
+            greeting = ""
+        for order in self.orders_list:
+            print(f"{greeting} {order.to_string()}")
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from Person import Person
 from Order import Order
 from CoffeeBar import CoffeeBar
+from Barista import Barista
 
 # Create 3 instances of customers
 amy = Person("Amy", "Coffee")
@@ -9,6 +10,20 @@ cat = Person("Cat", "Milk")
 
 # Create an instance of CoffeeBar
 coffee_bar = CoffeeBar("Rocko Brewing")
+
+# Place orders
+coffee_bar.place_order(amy.my_order())
+coffee_bar.place_order(bob.my_order())
+coffee_bar.place_order(cat.my_order())
+
+# Process orders
+coffee_bar.process_orders()
+
+# Instance of Barista, Kevin
+kevin = Barista("Kevin", "Espresso", "Hey dude!")
+
+# Instance of CoffeeBar with Kevin as the barista
+coffee_bar = CoffeeBar("Rocko Brewing", kevin)
 
 # Place orders
 coffee_bar.place_order(amy.my_order())
